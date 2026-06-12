2af1a9b9b668b5c98d3af08ac8bf839cd46e5965
rasyiqi-code <support@crediblemark.com>
2026-06-12 13:24:38 +0700

    style: hapus overflow-x-clip pada detail layanan index agar lebar sticky cta tidak melampaui scrollbar

--- components/public/service-detail/index.tsx
+++ components/public/service-detail/index.tsx
@@ -47,7 +47,7 @@ export function ServiceDetailContent({ service, isId, trustedAvatars = [] }: Ser
     };
 
     return (
-        <div className="relative min-h-screen bg-black overflow-x-clip flex flex-col">
+        <div className="relative min-h-screen bg-black flex flex-col">
             {/* Shared Background Pattern */}
             <div className="absolute inset-0 pointer-events-none overflow-hidden">
                 <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
